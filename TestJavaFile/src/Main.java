62485a0c940058083d6f54bffb762262a506c323
NiccyNet1989 <143370847+NiccyNet1989@users.noreply.github.com>
2025-03-15 00:08:12 -0700

    Created test java file
    Added test java file to repository to test adding code file.
    
    *Note
    I prefer Java but I'm open to using any language you guys want, let me know which one you guys prefer best!

---
+++ TestJavaFile/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello group!" +
+                "\nHere is a basic Java File just to test adding one to our Github Repository" +
+                "\nWe don't have to use Java if you don't want, we can use any language you prefer!");
+    }
+}
